c8bb7ce83de233137bd911bf6bddbc788f43b6e8
JAYESH SONI <67512410+thejayeshsoni@users.noreply.github.com>
2022-11-23 16:39:06 +0530

    Update cookieToken.js

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -2,7 +2,7 @@ const cookieToken = (user, res) => {
     const token = user.getJWT();
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true // This flag has used so that, only the server can manipulate cookies.
     }
 
     user.password = undefined;
